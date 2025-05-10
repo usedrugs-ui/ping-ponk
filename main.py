d9eddd081ee12eef0c437329eaab6328e8417f13
usedrugs-ui <dolgorfil@gmail.com>
2025-05-10 14:30:41 +0800

    main.py

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
 from pygame import *
 
-
-class GameSprite(sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
-       super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
-       self.speed = player_speed
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
-
-    def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
-    
-
-class Player(GameSprite):
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-   def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-
 background = (200, 255, 255)
 win_width = 800
 win_height = 500
@@ -38,5 +10,4 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
-
 
