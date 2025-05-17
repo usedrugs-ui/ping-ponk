1735275405418e86677294fc43fe4d02b86913c7
usedrugs-ui <dolgorfil@gmail.com>
2025-05-17 14:33:05 +0800

    racket.py

--- ping-pong/racket.py
+++ ping-pong/racket.py
@@ -3,7 +3,7 @@ from pygame import *
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
        super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.image = transform.scale(image.load(player_image), (wight, height))
        self.speed = player_speed
        self.rect = self.image.get_rect()
        self.rect.x = player_x
